45209ff319e4946e93a958dce1881f908fc83e1b
Rodrigo <rodrigo.viegas@ulbra.inf.br>
2020-03-08 21:39:36 -0300

    Date formated

--- lib/controller/cases_controller.dart
+++ lib/controller/cases_controller.dart
@@ -2,6 +2,7 @@ import 'package:coronavirus_tracker/models/cases.dart';
 import 'package:coronavirus_tracker/models/cases_confirmed.dart';
 import 'package:coronavirus_tracker/models/cases_suspected.dart';
 import 'package:coronavirus_tracker/repositories/cases_repository.dart';
+import 'package:intl/intl.dart';
 import 'package:mobx/mobx.dart';
 part 'cases_controller.g.dart';
 
@@ -16,7 +17,7 @@ abstract class _CasesControllerBase with Store {
 
   @observable
   ObservableList<Cases> listCases;
-  
+
   @observable
   ObservableList<CasesSuspected> listCasesSuspected;
 
@@ -33,7 +34,7 @@ abstract class _CasesControllerBase with Store {
   bool get isDataCasesConfirmed => listCasesConfirmed != null;
 
   @computed
-  String get getDate => 'Last Update: ${listCases[0].date}';
+  String get getDate => 'Last Update: ${_formatDate(listCases[0].date)}';
 
   @computed
   int get getDataCases => listCases[0].cases;
@@ -51,11 +52,18 @@ abstract class _CasesControllerBase with Store {
 
   @action
   loadCasesSuspected() async {
-    listCasesSuspected = ObservableList<CasesSuspected>.of(await _casesRepository.getCasesSuspected());
+    listCasesSuspected = ObservableList<CasesSuspected>.of(
+        await _casesRepository.getCasesSuspected());
   }
 
   @action
   loadCasesConfirmed() async {
-    listCasesConfirmed = ObservableList<CasesConfirmed>.of(await _casesRepository.getCasesConfirmed());
+    listCasesConfirmed = ObservableList<CasesConfirmed>.of(
+        await _casesRepository.getCasesConfirmed());
+  }
+
+  String _formatDate(String date) {
+    List<String> args = date.split('');
+    return '${args[8]}${args[9]}/${args[5]}${args[6]}/${args[0]}${args[1]}${args[2]}${args[3]} ${args[11]}${args[12]}:${args[14]}${args[15]}:${args[17]}${args[18]}';
   }
 }
